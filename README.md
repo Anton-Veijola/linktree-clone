217077e1763e7912cd29078acde6bd7d23b44423
Anton Veijola <antonv1997@gmail.com>
2025-02-10 16:12:36 +0200

    add auth

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ gems:
 devise (user auth)
 optional: tailwind_devise (auth tailwind styles)
 
-gem installation: bundle add devise tailwind_devise
+gem bundling:
+bundle add devise tailwind_devise
 
-devise installatin: rails g devise:install
+gem installing: 
+rails g devise:install
+rails g tailwind_devise:views
+
+models:
+user
+link
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :links
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  get "/:user_id", to: "links#user_links"
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "links#index"
 end
